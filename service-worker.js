5f8ff0e65d2c370969ecc764fb597e72fdd54521
Sreejith S <sreejithsureshkumar7@gmail.com>
2026-06-28 12:31:02 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,8 +7,8 @@ const APP_FILES = [
   "./firebase.js",
   "./manifest.json",
   "./offline.html",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
